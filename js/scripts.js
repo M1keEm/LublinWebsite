576a6bc69fc96116022a3b218c0123304dc386f4
MikeEm <michalbanaszek03@gmail.com>
2024-05-27 16:21:17 +0200

    trybunalska.html - style v1.0

--- js/scripts.js
+++ js/scripts.js
@@ -83,12 +83,15 @@ function fadeIn(el, display) {
 let map;
 
 async function initMap() {
-    const { Map } = await google.maps.importLibrary("maps");
-
+    const {Map} = await google.maps.importLibrary("maps");
     map = new Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
+        center: {lat: -34.397, lng: 150.644},
         zoom: 8,
     });
+    // const marker = new AdvancedMarkerElement({
+    //     map,
+    //     position: {lat: 37.4239163, lng: -122.0947209},
+    // });
 }
 
 initMap();
