7e3f6c5fade7aeb3ced2d04876f63698dd2d1208
MikeEm <michalbanaszek03@gmail.com>
2024-05-27 22:02:13 +0200

    image slider on trybunalska.html

--- js/scripts.js
+++ js/scripts.js
@@ -99,3 +99,21 @@ async function initMap() {
 }
 
 initMap();
+
+// img slider
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev">&lt;</button>',
+        nextArrow: '<button type="button" class="slick-next">&gt;</button>'
+    });
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true
+    });
+});
+
+
